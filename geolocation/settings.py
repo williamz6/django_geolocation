dab8f745961231d50f946664498dab1cb79805b5
otuekong <otuwilliam6@gmail.com>
2022-11-14 22:29:16 +0100

    secret key

--- geolocation/settings.py
+++ geolocation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fc$hn(p-)8l#)48&_*f#n#c6%1_7$7$@f%9g1)16-oo-k-myfj'
+SECRET_KEY = os.environ.get('DJANGO_GEO_LOCATION_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
